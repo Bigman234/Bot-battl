e5354d32a12078e1f24f8081e34ddb3c4fc816a9
mark-s-bit <markchege03@gmail.com>
2024-08-11 22:01:56 +0300

    intial

---
+++ src/YourBotArmy.js
@@ -0,0 +1,24 @@
+import React from "react";
+import BotCard from "./BotCard";
+import { ssrExportAllKey } from "vite/runtime";
+
+function YourBotArmy({ bots, releaseBot, dischargeBot }) {
+  return (
+    <div className="ui segment inverted teal bot-army">
+      <div className="ui five column grid">
+        <div className="row bot-army-row">
+          {bots.map((bot) => (
+            <BotCard
+              key={bot.id}
+              bot={bot}
+              handleClick={() => releaseBot(bot)}
+              handleDischarge={() => dischargeBot(bot)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default YourBotArmy;
