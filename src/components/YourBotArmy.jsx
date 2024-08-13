e005d480d639b64646f39cfd2655e076cf81afa8
mark-s-bit <markchege003@gmail.com>
2024-08-13 15:05:47 +0300

    Update YourBotArmy.jsx

--- src/components/YourBotArmy.jsx
+++ src/components/YourBotArmy.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-import { ssrExportAllKey } from "vite/runtime";
+import BotCard from "./BotCard";
 
-function YourBotArmy({ bots, releaseBot, dischargeBot }) {
+function YourBotArmy({ army, removeBot, dischargeBot }) {
   return (
-    <div className="ui segment inverted teal bot-army">
-      <div className="ui five column grid">
-        <div className="row bot-army-row">
-          {bots.map((bot) => (
-            <BotCard
-              key={bot.id}
-              bot={bot}
-              handleClick={() => releaseBot(bot)}
-              handleDischarge={() => dischargeBot(bot)}
-            />
-          ))}
-        </div>
-      </div>
+    <div className="your-bot-army">
+      <h2>Your Bot Army</h2>
+      {army.map((bot) => (
+        <BotCard
+          key={bot.id}
+          bot={bot}
+          handleClick={() => removeBot(bot)}
+          handleDischarge={() => dischargeBot(bot)}
+        />
+      ))}
     </div>
   );
 }
